0dcf8722312e080c08f3fcfb0052afe585004a08
ehab akl <ehabakl2023@mail.com>
2025-09-22 03:38:11 +0300

    updated

--- middleware.ts
+++ middleware.ts
@@ -58,5 +58,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/cart', '/wishList', '/products', '/categories', '/brands'],
+  matcher: ['/', '/cart', '/wishList', '/products', '/categories', '/brands' , "/allorders"],
 }
