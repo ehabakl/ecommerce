5c3c7f9d65a733ce117257fee9625c447d602095
ehab akl <ehabakl2023@mail.com>
2025-09-22 03:30:53 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import CategoriesData from "@/components/CategoriesData";
 import Landing from "@/components/Landing";
 import Products from "./products/page";
-import { getServerSession } from "next-auth";
-import { options } from "./api/auth/[...nextauth]/route";
+
 
 
 export default async function Home() {
 
-  const session = await getServerSession(options)
   return (
   <> 
   
